a6840ab0023aaa561161d225380b6ffc04df4eb8
SK Sajed <sajedsk177242@gmail.com>
2025-05-13 11:50:40 +0530

    CSS change

--- app/sign-in/[[...sign-in]]/page.jsx
+++ app/sign-in/[[...sign-in]]/page.jsx
@@ -1,7 +1,7 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <div className='fex h-screen items-center justify-center'>
+  return <div className='flex h-screen items-center justify-center w-full'>
     <SignIn />
   </div>
 }
